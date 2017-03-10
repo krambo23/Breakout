cbb49f7a61f2aec91401765a7636cf1218b9381c
krambo23 <taj.keshav@gmail.com>
2017-03-10 12:35:14 +0400

    Step 3

--- script.js
+++ script.js
@@ -25,11 +25,12 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 function drawBall()
 {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI * 2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -39,6 +40,17 @@ function draw()
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clears the canvas
 	drawBall(); // draws the ball
+
+	if ((x + dx) > (canvas.width - ballRadius) || (x + dx) < ballRadius)
+	{
+		dx = -dx;
+	}
+
+	if (((y + dy) > (canvas.height - ballRadius) || (y + dy) < ballRadius))
+	{
+		dy = -dy;
+	}
+
 	x += dx; // increments x by dx
 	y += dy; // increments y by dy
 }
